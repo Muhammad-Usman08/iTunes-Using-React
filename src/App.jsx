b7626587da72d9dc3be62d8e8991000036573fc7
musman-js <usman2037966@gmail.com>
2024-04-24 17:16:13 +0500

    navbar started

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './style.css'
+
+
+function App(){
+  return(
+    <>
+      <nav className='navbar navbar-expand-lg mynavbar'>
+        <div className='container flex justify-center pt-3 pb-3'>
+          <h4 className='heading1'>myTunes</h4>
+          <h4 className='heading1'>Overview</h4>
+          <h4 className='heading1'>Music</h4>
+          <h4 className='heading1'>Gift Card</h4>
+          <h4 className='heading1'>Video</h4>
+        </div>
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+
+        </div>
+      </nav>
+           
+    </>
+  )
+}
+
+export default App
