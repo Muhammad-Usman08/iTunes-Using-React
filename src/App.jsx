e5bdb4a8b15e2314f2de20e8303dec523c52b57c
musman-js <usman2037966@gmail.com>
2024-04-26 21:47:50 +0500

    section 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './style.css'
 
 
-function App(){
+  function App(){
   return(
     <>
-      <nav className='navbar navbar-expand-lg mynavbar'>
-        <div className='container flex justify-center pt-3 pb-3'>
+      <nav className='navbar navbar-expand-lg'>
+        <div className='flex justify-center pt-3 pb-3 bg-[#333333]'>
           <h4 className='heading1'>myTunes</h4>
           <h4 className='heading1'>Overview</h4>
           <h4 className='heading1'>Music</h4>
@@ -23,6 +23,18 @@ function App(){
           <h2 className='text-6xl font-bold'>myTunes.</h2>
           <h4 className='text-3xl mt-10 font-semibold'>Your music, movies, and TV shows take center stage</h4>
           <p className='container text-[20px] mt-6 mx-auto'>myTunes is the best way to organize and enjoy the music, movies, and TV shows you already have — and shop for the ones you want. Enjoy all the entertainment myTunes has to offer on your Mac and PC.</p>
+        </div>
+      </section>
+
+
+      <section>
+        <div className='bg-[#252529] text-white text-center px-10 pt-10'> 
+          <h2 className='text-5xl font-bold'> <img src="" alt="" /> Music</h2>
+          <h3 className='text-[30px] mt-10 font-semibold'>50 million songs.Zero ads.</h3>
+          <p className='text-[20px] mt-10 font-semibold'>Stream over 50 million songs, ad-free. Or download albums and tracks to listen to offline. All the music in your personal myTunes library — no matter where it came from — lives right alongside the Orange Music catalog. Start your free three-month trial with no commitment, and cancel anytime.</p>
+          <button className='bg-[#b90415] px-4 py-4 hover:bg-red-600 mt-10'>Start Your Trial Now</button>
+          <p className='mt-10'>Orange Music is available in myTunes, and for iOS and Android devices.</p>
+          <img className='music-image mt-16' src="https://thomasmartin86.github.io/mytuneslanding/img/mockup1.png" alt="" />
         </div>
       </section>
            
